01e2bcbce3e72fc2c417bf9f27fe571b1c8995b0
ocram1905 <123321528+ocram1905@users.noreply.github.com>
2024-06-17 17:26:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const passwordInput = document.getElementById('password');
     const passwordError = document.getElementById('password-error');
     
-    const correctPassword = "13/02/2022"; // Imposta la password corretta qui
+    const correctPassword = "your_password"; // Imposta la password corretta qui
 
     protectedButton.addEventListener('click', function() {
         passwordPrompt.style.display = 'block';
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     submitPassword.addEventListener('click', function() {
         if (passwordInput.value === correctPassword) {
-            window.location.href = 'video4.mp4'; // Sostituisci 'path_to_video4' con il percorso effettivo
+            window.location.href = 'videos/video4.mp4'; // Percorso corretto del video 4
         } else {
             passwordError.style.display = 'block';
         }
