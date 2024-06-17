3656299c883192e63461eb3c38aff38f017439ad
ocram1905 <123321528+ocram1905@users.noreply.github.com>
2024-06-17 17:34:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const downloadButton = document.getElementById('download-button');
+    const protectedButton = document.getElementById('download-button');
+    const passwordPrompt = document.getElementById('password-prompt');
+    const submitPassword = document.getElementById('submit-password');
+    const passwordInput = document.getElementById('password');
+    const passwordError = document.getElementById('password-error');
+    
+    const correctPassword = "your_password"; // Imposta la password corretta qui
 
-    downloadButton.addEventListener('click', function() {
-        window.location.href = 'videos/video4.mp4'; // Percorso corretto del video 4
+    protectedButton.addEventListener('click', function() {
+        passwordPrompt.style.display = 'block';
+    });
+
+    submitPassword.addEventListener('click', function() {
+        if (passwordInput.value === correctPassword) {
+            window.location.href = 'videos/video4.mp4'; // Percorso corretto del video 4
+        } else {
+            passwordError.style.display = 'block';
+        }
     });
 });
