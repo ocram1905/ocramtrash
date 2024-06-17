11936886a750aca1282add923e50e05d3f9f2b41
ocram1905 <123321528+ocram1905@users.noreply.github.com>
2024-06-17 10:48:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 /* script.js */
 document.addEventListener("DOMContentLoaded", function() {
-    const gridItems = document.querySelectorAll('.grid-item');
+    const videoItems = document.querySelectorAll('.video-item');
     const videoPlayer = document.querySelector('.video-player');
-    const videoElement = document.getElementById('video-player');
+    const mainVideo = document.getElementById('main-video');
     const videoSource = document.getElementById('video-source');
 
-    gridItems.forEach(item => {
+    videoItems.forEach(item => {
         item.addEventListener('click', function() {
             const videoSrc = this.getAttribute('data-video');
             videoSource.src = videoSrc;
-            videoElement.load();
-            videoElement.play();
+            mainVideo.load();
+            mainVideo.play();
             videoPlayer.classList.add('active');
         });
     });
 
     videoPlayer.addEventListener('click', function() {
-        videoElement.pause();
+        mainVideo.pause();
         videoPlayer.classList.remove('active');
     });
 });
